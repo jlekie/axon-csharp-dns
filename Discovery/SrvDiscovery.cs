ba607ab91ec117796c1644f95a8547152c9294d9
Joe Lekie <joe.lekie@greenheck.com>
2019-12-18 16:35:38 -0600

    Checkpoint

---
+++ Discovery/SrvDiscovery.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using DnsClient;
+
+namespace Axon.Dns
+{
+    public class SrvDiscoverer<TEndpoint> : ADiscoverer<TEndpoint> where TEndpoint : IEncodableEndpoint
+    {
+        private LookupClient dnsClient;
+        private Random discoveryRand;
+
+        public SrvDiscoverer(string identifier, IEndpointDecoder<TEndpoint> endpointDecoder)
+            : base(identifier, endpointDecoder)
+        {
+            this.dnsClient = new LookupClient();
+            this.discoveryRand = new Random();
+        }
+
+        public override async Task<TEndpoint> Discover(int timeout = 0)
+        {
+            var results = await this.dnsClient.QueryAsync(this.Identifier, QueryType.SRV);
+            var srvRecords = results.Answers.SrvRecords().ToArray();
+            var srvRecord = srvRecords[this.discoveryRand.Next(srvRecords.Length)];
+
+            return this.EndpointDecoder.Create(srvRecord.Target, srvRecord.Port); //TODO
+        }
+        public override async Task<TEndpoint[]> DiscoverAll(int timeout = 0)
+        {
+            var results = await this.dnsClient.QueryAsync(this.Identifier, QueryType.SRV);
+            var srvRecords = results.Answers.SrvRecords().ToArray();
+
+            return srvRecords.Select(r => this.EndpointDecoder.Create(r.Target, r.Port)).ToArray(); // TODO
+        }
+        public override Task Blacklist(TEndpoint endpoint)
+        {
+            Console.WriteLine("BLACKLISTING " + Encoding.UTF8.GetString(endpoint.Encode()));
+
+            return Task.FromResult(true);
+        }
+    }
+}
